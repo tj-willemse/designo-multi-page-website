966a96fc22e14325987e6ddc31ed3f38c26e72f6
xonic1 <tjwillemse1@gmail.com>
2024-10-16 20:22:25 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/modern.css';
+import '../styles/style.css';
+import '../styles/components/index.css';
+import '../styles/components/header.css';
+import '../styles/components/footer.css';
+import '../styles/util.css';
